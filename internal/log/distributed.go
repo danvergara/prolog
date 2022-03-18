14e4f949ea9493c64ae4b5c6cdc40bbe9253501a
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2022-03-18 00:11:51 -0600

    add the fsm logic

--- internal/log/distributed.go
+++ internal/log/distributed.go
@@ -1,15 +1,19 @@
 package log
 
 import (
+	"bytes"
 	"crypto/tls"
+	"fmt"
 	"io"
 	"net"
 	"os"
 	"path/filepath"
 	"time"
 
+	api "github.com/danvergara/prolog/api/v1"
 	"github.com/hashicorp/raft"
 	raftboltdb "github.com/hashicorp/raft-boltdb"
+	"google.golang.org/protobuf/proto"
 )
 
 // DistributedLog that's replicated on multiple computers.
@@ -29,14 +33,18 @@ func NewDistributedLog(dataDir string, config Config) (*DistributedLog, error) {
 		config: config,
 	}
 
-	if err := l.setup(dataDir); err != nil {
+	if err := l.setupLog(dataDir); err != nil {
+		return nil, err
+	}
+
+	if err := l.setupRaft(dataDir); err != nil {
 		return nil, err
 	}
 
 	return l, nil
 }
 
-func (l *DistributedLog) setup(dataDir string) error {
+func (l *DistributedLog) setupLog(dataDir string) error {
 	logDir := filepath.Join(dataDir, "log")
 	if err := os.MkdirAll(logDir, 0755); err != nil {
 		return err
@@ -134,14 +142,111 @@ func (l *DistributedLog) setupRaft(dataDir string) error {
 	return err
 }
 
+// Append appends the record to the log. Unlike in Code the Store,
+// where we append the record directly to this server's log, we tell
+// Raft to apply to a command (we've reused for the ProduceRequest for the
+// command) that tells the FSM to append the record to the log.
+// Raft runs the process described in Log Replication,
+// to replicate the command to a majority of the Raft servers
+// and ultimately append the record to a majority of Raft Servers.
+func (l *DistributedLog) Append(record *api.Record) (uint64, error) {
+	res, err := l.apply(
+		AppendRequestType,
+		&api.ProduceRequest{Record: record},
+	)
+	if err != nil {
+		return 0, err
+	}
+
+	return res.(*api.ProduceResponse).Offset, nil
+}
+
+// apply wraps Raft's API to apply requests and return their responses.
+// Even though we have only one request type, the append request type,
+// I've written things that easily support multiple request types to show you
+// would set up your own services when you have different requests.
+// In apply(), we marshal the request type and request into bytes that
+// Raft uses as the record's data it replicates.
+// The l.raft.Apply(buf.Bytes(), timeout) call has a lot going on behind the scenes,
+// running the steps described in Log Replication, to replicate the record and append
+// the record to the leader's log.
+func (l *DistributedLog) apply(reqType RequestType, req proto.Message) (interface{}, error) {
+	var buf bytes.Buffer
+	_, err := buf.Write([]byte{byte(reqType)})
+	if err != nil {
+		return nil, err
+	}
+
+	b, err := proto.Marshal(req)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = buf.Write(b)
+	if err != nil {
+		return nil, err
+	}
+
+	timeout := 10 * time.Second
+	future := l.raft.Apply(buf.Bytes(), timeout)
+	if future.Error() != nil {
+		return nil, future.Error()
+	}
+
+	res := future.Response()
+	if err, ok := res.(error); ok {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+// Read reads the record for the offset from the server's log.
+// When you're ok with relaxed consistency, read operations
+// need not to go through Raft.
+func (l *DistributedLog) Read(offset uint64) (*api.Record, error) {
+	return l.log.Read(offset)
+}
+
+// RequestType type.
+type RequestType uint8
+
+const (
+	// AppendRequestType const.
+	AppendRequestType RequestType = 0
+)
+
 var _ raft.FSM = (*fsm)(nil)
 
+// Raft defers the running of your business logic to the FSM.
 type fsm struct {
 	log *Log
 }
 
 func (f *fsm) Apply(record *raft.Log) interface{} {
+	buf := record.Data
+	reqType := RequestType(buf[0])
+
+	switch reqType {
+	case AppendRequestType:
+		return f.applyAppend(buf[1:])
+	}
+
 	return nil
+}
+
+func (f *fsm) applyAppend(b []byte) interface{} {
+	var req api.ProduceRequest
+	err := proto.Unmarshal(b, &req)
+	if err != nil {
+		return err
+	}
+	offset, err := f.log.Append(req.Record)
+	if err != nil {
+		return err
+	}
+
+	return &api.ProduceResponse{Offset: offset}
 }
 
 func (f *fsm) Snapshot() (raft.FSMSnapshot, error) {
@@ -151,6 +256,34 @@ func (f *fsm) Snapshot() (raft.FSMSnapshot, error) {
 }
 
 func (f *fsm) Restore(r io.ReadCloser) error {
+	b := make([]byte, lenWidth)
+	var buf bytes.Buffer
+	for i := 0; ; i++ {
+		_, err := io.ReadFull(r, b)
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return err
+		}
+		size := int64(enc.Uint64(b))
+		if _, err = io.CopyN(&buf, r, size); err != nil {
+			return err
+		}
+		record := &api.Record{}
+		if err = proto.Unmarshal(buf.Bytes(), record); err != nil {
+			return err
+		}
+		if i == 0 {
+			f.log.Config.Segment.InitialOffset = record.Offset
+			if err := f.log.Reset(); err != nil {
+				return err
+			}
+		}
+		if _, err = f.log.Append(record); err != nil {
+			return err
+		}
+		buf.Reset()
+	}
 	return nil
 }
 
@@ -161,6 +294,10 @@ type snapshot struct {
 }
 
 func (s *snapshot) Persist(sink raft.SnapshotSink) error {
+	if _, err := io.Copy(sink, s.reader); err != nil {
+		_ = sink.Cancel()
+		return err
+	}
 	return nil
 }
 
@@ -190,20 +327,40 @@ func (l *logStore) LastIndex() (uint64, error) {
 }
 
 func (l *logStore) GetLog(index uint64, out *raft.Log) error {
+	in, err := l.Read(index)
+	if err != nil {
+		return err
+	}
+
+	out.Data = in.Value
+	out.Index = in.Offset
+	out.Type = raft.LogType(in.Type)
+	out.Term = in.Term
 	return nil
 }
 
 func (l *logStore) StoreLog(record *raft.Log) error {
-	return nil
+	return l.StoreLogs([]*raft.Log{record})
 }
 
 func (l *logStore) StoreLogs(records []*raft.Log) error {
+	for _, record := range records {
+		if _, err := l.Append(&api.Record{
+			Value: record.Data,
+			Term:  record.Term,
+			Type:  uint32(record.Type),
+		}); err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
 func (l *logStore) DeleteRange(min, max uint64) error {
 	return l.Truncate(max)
 }
+
+var _ raft.StreamLayer = (*StreamLayer)(nil)
 
 // StreamLayer type.
 // Raft uses a  stream layer in the transport to provide a low-level
@@ -214,21 +371,105 @@ type StreamLayer struct {
 	peerTLSConfig   *tls.Config
 }
 
+// NewStreamLayer returns a pointer to a StreamLayer instance.
+func NewStreamLayer(ln net.Listener, serverTLSConfig, peerTLSConfig *tls.Config) *StreamLayer {
+	return &StreamLayer{
+		ln:              ln,
+		serverTLSConfig: serverTLSConfig,
+		peerTLSConfig:   peerTLSConfig,
+	}
+}
+
+// RaftRPC byte to identify the connection type.
+const RaftRPC = 1
+
+// Accept is the mirror of Dial. We accept the incoming connection
+// and read the byte that identifies the connection and then create
+// a server-side TLS connection.
 func (s *StreamLayer) Accept() (net.Conn, error) {
-	conn, _ := s.ln.Accept()
+	conn, err := s.ln.Accept()
+	if err != nil {
+		return nil, err
+	}
+	b := make([]byte, 1)
+	_, err = conn.Read(b)
+	if err != nil {
+		return nil, err
+	}
+	if bytes.Compare([]byte{byte(RaftRPC)}, b) != 0 {
+		return nil, fmt.Errorf("not a raft rpc")
+	}
+	if s.serverTLSConfig != nil {
+		return tls.Server(conn, s.serverTLSConfig), nil
+	}
 	return conn, nil
 }
 
+// Addr returns the listener's address.
 func (s *StreamLayer) Addr() net.Addr {
 	return s.ln.Addr()
 }
 
+// Close closes the listener listener.
 func (s *StreamLayer) Close() error {
 	return s.ln.Close()
 }
 
+// Dial makes outgoing connections to other servers in the Raft cluster.
+// When we connect to a server, we write the RaftRPC byte to identify the connection
+// type so we can multiplex Raft on the same port as our Log gRPC requests.
+// If we configure the stream layer with a peer TLS config, we make a TLS client-side
+// connection.
 func (s *StreamLayer) Dial(addr raft.ServerAddress, timeout time.Duration) (net.Conn, error) {
 	dialer := &net.Dialer{Timeout: timeout}
 	var conn, err = dialer.Dial("tcp", string(addr))
+	if err != nil {
+		return nil, err
+	}
+	// identify to mux this is a raft rpc.
+	_, err = conn.Write([]byte{byte(RaftRPC)})
+	if err != nil {
+		return nil, err
+	}
+	if s.peerTLSConfig != nil {
+		conn = tls.Client(conn, s.peerTLSConfig)
+	}
+
 	return conn, err
+}
+
+// Join adds the server to the Raft cluster. We add every server as a voter,
+// but Raft supports adding servers as non-voters with the AddNonVoter() API.
+func (l *DistributedLog) Join(id, addr string) error {
+	configFuture := l.raft.GetConfiguration()
+	if err := configFuture.Error(); err != nil {
+		return err
+	}
+	serverID := raft.ServerID(id)
+	serverAddr := raft.ServerAddress(addr)
+	for _, srv := range configFuture.Configuration().Servers {
+		if srv.ID == serverID || srv.Address == serverAddr {
+			if srv.ID == serverID && srv.Address == serverAddr {
+				// server has already joined.
+				return nil
+			}
+			// remove the existing server
+			removeFuture := l.raft.RemoveServer(serverID, 0, 0)
+			if err := removeFuture.Error(); err != nil {
+				return err
+			}
+		}
+	}
+	addFuture := l.raft.AddVoter(serverID, serverAddr, 0, 0)
+	if err := addFuture.Error(); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Leave removes the server from the cluster.
+// Removing the lider will trigger a new election.
+func (l *DistributedLog) Leave(id string) error {
+	removeFuture := l.raft.RemoveServer(raft.ServerID(id), 0, 0)
+	return removeFuture.Error()
 }
